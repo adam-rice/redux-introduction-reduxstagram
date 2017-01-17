9cf7765a8ed7caab5fa24440987042c201a1f326
Adam Rice <adamcoltonrice@gmail.com>
2017-01-17 16:54:23 -0700

    integrate store with main.js

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -3,18 +3,22 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
 import css from './styles/style.styl';
+import store, { history } from './store.js';
 import Main from './components/Main.js';
 import Single from './components/Single.js';
 import Photogrid from './components/Photogrid.js';
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={Photogrid}></IndexRoute>
-      <Route path="/view/:postId" component={Single}></Route>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={Photogrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
 );
 
 
