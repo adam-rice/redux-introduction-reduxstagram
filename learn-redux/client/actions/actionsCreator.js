e003a5135abf1e8e9060cb9636e94a7a516a6e7c
Adam Rice <adamcoltonrice@gmail.com>
2017-01-17 16:25:49 -0700

    redux actions setup

---
+++ learn-redux/client/actions/actionsCreator.js
@@ -0,0 +1,28 @@
+/*jshint esversion: 6 */
+
+//increment
+export function increment(index) {
+  return {
+    type: "INCREMENT_LIKES",
+    index //es6
+  };
+}
+
+//add comment
+export function addComment(postId, author, comment) {
+  return {
+    type: "ADD_COMMENT",
+    postId,
+    author,
+    comment
+  };
+}
+
+//remove comment
+export function removeComment(postId, i) {
+  return {
+    type: "REMOVE_COMMENT",
+    i,
+    postId
+  };
+}
