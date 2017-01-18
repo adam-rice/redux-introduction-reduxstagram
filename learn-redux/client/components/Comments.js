0bfdb55133bcf4c62cafb7428f7d1d382b2cb796
Adam Rice <adamcoltonrice@gmail.com>
2017-01-18 11:43:50 -0700

    displaying the single photo component & comments component setup

---
+++ learn-redux/client/components/Comments.js
@@ -0,0 +1,15 @@
+/*jshint esversion: 6 */
+
+import React,{ Component } from 'react';
+
+
+export default class Comments extends Component {
+  render() {
+    return (
+      <div className="comment">
+        wowowow
+      </div>
+
+    );
+  }
+}
--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,13 +1,20 @@
 /*jshint esversion: 6 */
 
 import React,{ Component } from 'react';
+import Photo from './Photo.js';
+import Comments from './Comments.js';
+
+//import comments
 
 
 export default class Single extends Component {
   render() {
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const post = this.props.posts[i];
     return (
       <div className="single-photo">
-        I'm the single component
+        <Photo i={i} post={post} {...this.props}/>
+        <Comments/>
       </div>
     );
   }
